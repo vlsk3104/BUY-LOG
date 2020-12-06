5289ef1486246f35ee373063a41c58597e3c802b
vlsk3104 <vlsk3104@gmail.com>
2020-12-06 03:29:10 +0000

    タイトル追加とレイアウト調整

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')  # full_titleメソッドを定義
+    base_title = 'バイログ'
+    if page_title.blank?
+      base_title  # トップページはタイトル「バイログ」
+    else
+      "#{page_title} - #{base_title}" # トップ以外のページはタイトル「利用規約 - バイログ」（例）
+    end
+  end
 end
