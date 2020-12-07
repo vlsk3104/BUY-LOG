16f6409a28f7a63370693766535455a8c86ab07a
vlsk3104 <vlsk3104@gmail.com>
2020-12-07 05:55:58 +0000

    ユーザーの基本機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  
  root 'static_pages#home'
  get :about,        to: 'static_pages#about'
  get :use_of_terms, to: 'static_pages#terms'
+ get :signup,       to: 'users#new'
+ resources :users
 end
