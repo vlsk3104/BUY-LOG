f79e950a5021f15db5c840728ce1954b2f5f3009
vlsk3104 <vlsk3104@gmail.com>
2020-12-07 06:05:22 +0000

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "クックログへようこそ！"
+      flash[:success] = "バイログへようこそ！"
       redirect_to @user
     else
       render 'new'
