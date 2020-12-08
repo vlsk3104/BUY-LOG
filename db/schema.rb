74574f55cfcfe4434274f1ad9a695e46bc1d172f
vlsk3104 <vlsk3104@gmail.com>
2020-12-08 05:12:44 +0000

    ユーザーの更新、一覧、削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_08_022807) do
+ActiveRecord::Schema.define(version: 2020_12_08_041607) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_12_08_022807) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
